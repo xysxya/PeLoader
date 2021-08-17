1561885b25432d04d5bc281872bba4fcdb478f99
xysx <1274860958@qq.com>
2021-08-17 16:44:47 +0800

    0817

--- main.c
+++ main.c
@@ -3,6 +3,80 @@
 #include <fileapi.h>
 #include <winnt.h>
 
+//set Entry
+BOOL CallEntry (char* chBaseAddress) {
+    PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)chBaseAddress;
+    PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)(chBaseAddress + pDos->e_lfanew);
+    char *ExeEntry = (char*)(chbaseAddress + pNt->OptionalHeader.AddressOfEntryPoint);
+    __asm {
+        mov eax, ExeEntry
+        jmp eax
+    }
+    return TRUE;
+}
+
+BOOL SetImageBase(char* chBaseAddress) {
+    PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)chBaseAddress;
+    PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)(chBaseAddress + pDos->e_lfanew);
+    pNt->OptionalHeader.ImageBase = (ULONG32)chBaseAddress;
+
+    return TRUE;
+}
+
+BOOL ImportTable(char* chBaseAddress) {
+    PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)chBaseAddress;
+    PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)(chBaseAddress + pDos->e_lfanew);
+    PIMAGE_IMPORT_DESCRIPTOR pImportTable = (PIMAGE_IMPORT_DESCRIPTOR)((DWORD)pDos +
+            pNt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
+
+    char *lpDllName = NULL;
+    HMODULE hDll = NULL;
+    PIMAGE_THUNK_DATA lpImportNameArray = NULL;
+    PIMAGE_IMPORT_BY_NAME lpImportByName = NULL;
+    PIMAGE_THUNK_DATA lpImportFuncAddrArray = NULL;
+    FARPROC lpFuncAddress = NULL;
+    DWORD i = 0;
+
+    while(TRUE) {
+        if (0 == pImportTable->OriginalFirstThunk) {
+            break;
+        }
+
+        lpDllName = (char*)((DWORD)pDos + pImportTable->Name);
+        hDll = GetModuleHandleA((lpDllName));
+        if (NULL == hDLL) {
+            hdll = LoadLibraryA(lpDllName);
+            if (NULL == hDll) {
+                pImportTable++;
+                continue;
+            }
+        }
+
+        i = 0;
+        lpImportNameArray = (PIMAGE_THUNK_DATA)((DWORD)pDos + pImportTable->OriginalFirstThunk);
+        lpImportFuncAddrArray = (PIMAGE_THUNK_DATA)((DWORD)pDos + pImportTable->FirstThunk);
+        while (TRUE) {
+            if (0 == lpImportNameArray[i].u1.AddressOfData) {
+                break;
+            }
+            lpImportByName = (PIMAGE_IMPORT_BY_NAME)((DWORD)pDos + lpImportNameArray[i].u1.AddressOfData);
+
+            if (0x80000000 & lpImportNameArray[i].u1.Ordinal) {
+                lpFuncAddress = GetProcAddress(hDll, (LPCSTR)(lpImportNameArray[i].u1.Ordinal & 0x0000FFFF));
+            }
+            else {
+                lpFuncAddress = GetProcAddress(hDll, (LPCSTR)lpImportByName->Name);
+            }
+            lpImportNameArray[i].u1.Function = (DWORD)lpFuncAddress;
+            i++;
+        }
+
+        pImportTable++;
+    }
+
+    return TRUE;
+
+}
 int main() {
     char szFilename[] = ".\\1.exe";
     HANDLE hFile = CreateFileA(
